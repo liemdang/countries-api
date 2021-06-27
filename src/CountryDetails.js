4c05f04c62d09e7dca0ef5369c353b56fdbbc1c1
liem <liem.dang@hotmail.com>
2021-06-27 17:02:34 +0200

    improved responsive design

--- src/CountryDetails.js
+++ src/CountryDetails.js
@@ -58,34 +58,35 @@ const CountryDetails = (props) => {
         <div className="countryDetails">
             <img className="countryDetails__flag" src={country.flag} alt={country.name} />
             <div className="countryDetails__description">
-                <h2 className="countryDetails__description-name">{country.name}</h2>
+                
                 <div className="countryDetails__description-container">
-                    <div className="countryDetails__description-column1">
+                    <div>
+                    <h2 className="countryDetails__description-name">{country.name}</h2>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Native Name: </span> {country.nativeName}
+                            <span className="countryDetails__description-criteria">Native Name:&nbsp; </span> {country.nativeName}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Population: </span>{country.population}
+                            <span className="countryDetails__description-criteria">Population:&nbsp;</span> {country.population}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Region: </span>{country.region}
+                            <span className="countryDetails__description-criteria">Region:&nbsp;</span> {country.region}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Sub Region: </span>{country.subregion}
+                            <span className="countryDetails__description-criteria">Sub Region:&nbsp;</span> {country.subregion}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Capital: </span>{country.capital}
+                            <span className="countryDetails__description-criteria">Capital:&nbsp;</span> {country.capital}
                         </p>
                     </div>
                     <div className="countryDetails__description-column2">
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Top Level Domain: </span>{country.topLevelDomain}
+                            <span className="countryDetails__description-criteria">Top Level Domain:&nbsp;</span>{country.topLevelDomain}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Currencies: </span>{country.currencies && country.currencies[0].name}
+                            <span className="countryDetails__description-criteria">Currencies:&nbsp;</span>{country.currencies && country.currencies[0].name}
                         </p>
                         <p className="countryDetails__description-info">
-                            <span className="countryDetails__description-criteria">Languages: </span>{country.languages && country.languages.map((language) => {
+                            <span className="countryDetails__description-criteria">Languages:&nbsp; </span>{country.languages && country.languages.map((language) => {
                                 return language.name;
                             })
                             .join(" ")
@@ -96,13 +97,15 @@ const CountryDetails = (props) => {
                     
                 </div>
                 {borderCountryNames.length > 0 ?
-                (<span className="countryDetails__description-info">
-                             <p className="countryDetails__description-criteria">Border Countries: </p>
+                (<div className="countryDetails__description-info countryDetails__bCountries">
+                             <p className="countryDetails__description-criteria bCountries">Border Countries: </p>
+                             <div>
                             {borderCountryNames.map(country => {
                                 return <button key={country} className="countryDetails__borderCountries">{country}</button>
                             })}
+                            </div>
                             
-                    </span>) : null}
+                    </div>) : null}
             </div>
 
         </div>
